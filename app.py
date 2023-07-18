eaed96ccbcbed35c85434e3b496807cd59096440
Tracy Wei <tracywei27@gmail.com>
2023-07-18 14:28:10 -0700

    added spellchecker

--- app.py
+++ app.py
@@ -4,16 +4,22 @@ from spellchecker import SpellChecker
 app = Flask(__name__, template_folder='templates')
 spell = SpellChecker()
 
-@app.route('/', methods =["GET", "POST"])
+
+@app.route('/', methods=["GET", "POST"])
 def getText():
     if request.method == "POST":
-       myText = request.form.get("myText")
-       wordList = myText.split()
-       misspelled = spell.unknown(wordList)
-       for word in misspelled:
-        print(spell.candidates(word))
-       return "Your text is: "+myText
+        myText = request.form.get("myText")
+        wordList = myText.split()
+        misspelled = spell.unknown(wordList)
+        corrected_text = ""
+        for word in wordList:
+            if word in misspelled:
+                corrected_text += '<span style="color: red;">' + word + '</span> '
+            else:
+                corrected_text += word + ' '
+        return render_template("index.html", corrected_text=corrected_text)
     return render_template("index.html")
+
 
 @app.route("/")
 def index():
@@ -21,4 +27,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=8080, debug=True)
