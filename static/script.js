42bf0fac5da145174a588169cae7aedf7ced8035
Tracy Wei <tracywei27@gmail.com>
2023-07-19 15:27:39 -0700

    added suggestion cards

---
+++ static/script.js
@@ -0,0 +1,12 @@
+// const correctedText = "{{ corrected_text | safe }}";
+// document.querySelector('edited-input').value = correctedText;
+
+// $(document).ready(function () {
+//   var suggestions = JSON.parse('{{ suggestions|safe }}');
+
+//   $(".misspelled").click(function () {
+//     var word = $(this).data("word");
+//     var suggestion = suggestions[word];
+//     $("#suggestionCard").text("Spelling mistake for " + word + ". You may mean: " + suggestion);
+//   });
+// });
