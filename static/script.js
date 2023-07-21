d0a5bb032968f35bbf40144a470d8af43c7b9df0
Tracy Wei <tracywei27@gmail.com>
2023-07-21 11:56:10 -0700

    transferred js to script.js

--- static/script.js
+++ static/script.js
@@ -1,23 +1,21 @@
-// const correctedText = "{{ corrected_text | safe }}";
-// document.querySelector('edited-input').value = correctedText;
+$(document).ready(function () {
+  var suggestions = suggestionsData;
+  var clickedWords = new Set(); // Store the already clicked misspelled words
 
-// $(document).ready(function () {
-//   var suggestions = JSON.parse('{{ suggestions|safe }}');
-
-//   $(".misspelled").click(function () {
-//     var word = $(this).data("word");
-//     var suggestion = suggestions[word];
-//     $("#suggestionCard").text("Spelling mistake for " + word + ". You may mean: " + suggestion);
-//   });
-// });
-
-var suggestions = JSON.parse('{{ suggestions|safe }}');
-
-for (let s of suggestions) {
-  console.log(s);
-  var elem = document.querySelector("#suggestionCard").cloneNode(true);
-  elem.classList.remove("hidden");
-  elem.querySelector("#error").textContent = "Spelling mistake for " + s;
-  elem.querySelector("#suggestions").textContent = "You may mean: " + suggestions[s];
-  document.querySelector("#suggContainer").append(elem);
-}
+  $("#textarea").on("click", ".misspelled", function () {
+    var word = $(this).data("word");
+    if (!clickedWords.has(word)) {
+      clickedWords.add(word);
+      var suggestion = suggestions[word];
+      var elem = document.querySelector("#suggestionCard").cloneNode(true);
+      elem.classList.remove("hidden");
+      elem.querySelector("#error").textContent = "Spelling mistake for " + word;
+      elem.querySelector("#suggestions").textContent = "You may mean: " + suggestion;
+      elem.querySelector(".suggButton").addEventListener("click", function () {
+        elem.remove();
+        $(".misspelled[data-word='" + word + "']").removeClass("misspelled");
+      });
+      document.querySelector("#suggContainer").append(elem);
+    }
+  });
+});
