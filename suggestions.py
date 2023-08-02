18c5e2e3018f299d0e72758f740646014b4568f5
Tracy Wei <tracywei27@gmail.com>
2023-08-02 16:49:14 -0700

    added suggestions and similarity files

--- suggestions.py
+++ suggestions.py
@@ -1,7 +1,17 @@
 import spacy
 nlp = spacy.load("en_core_web_sm")
 
+def applySugg(text, context):
+  return text
+
+def identityPpl(text):
+  if text.index('woman') > -1 or text.index('man') > -1 or text.index('person') > -1:
+    suggestNames(text)
+
 def suggestNames(context):
   doc = nlp(context)
   names = [ent.text for ent in doc.ents if ent.label_ == 'PERSON']
   return set(names)
+
+def checkNumber(text):
+  return text
