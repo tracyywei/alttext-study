65293c32903278a1b79f1247b720de9551a79f9f
Tracy Wei <tracywei27@gmail.com>
2023-08-11 14:50:20 -0700

    submitted edits save to json file

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pandas as pd
 from flask import Flask, request, render_template
 from preprocessing import preprocess
 from suggestions import applySugg
+import time
 
 app = Flask(__name__, template_folder='templates')
 data_folder = os.path.join(app.root_path, 'data')  # Path to the 'data' folder
@@ -21,9 +22,11 @@ def get_random_data():
     caption = random_row['caption'].values[0]
     return image_url, blip_beam_text, context, article_name, caption
 
+
 @app.route('/', methods=["GET", "POST"])
 def getText():
     image_url, blip_beam_text, context, article_name, caption = get_random_data()
+    start_time = time.time()
 
     # preprocessing
     myText = preprocess(blip_beam_text)
@@ -32,12 +35,38 @@ def getText():
     corrected_text, suggestions = applySugg(myText, context)
 
     if request.method == "POST":
+
+        orig_text = request.form.get("ogText", "")
         edited_text = request.form.get("editedText", "")
-        print(edited_text)
-        with open("data/saved.txt", "w") as file:
-            file.write(blip_beam_text)
+        image = request.form.get("ogImg", "")
+
+        end_time = time.time()
+
+        dictionary = {
+            "start_time": start_time,
+            "end_time": end_time,
+            "orig_text": orig_text,
+            "edited_text": edited_text,
+            "image": image
+        }
+        
+        # Writing to json
+        saved_file_path = os.path.join(data_folder, 'sample.json')
+        
+        # Load existing data from the file if available
+        existing_data = []
+        if os.path.exists(saved_file_path):
+            with open(saved_file_path, "r") as infile:
+                existing_data = json.load(infile)
+        
+        # Append the new data entry
+        existing_data.append(dictionary)
+        
+        # Write the updated data back to the file
+        with open(saved_file_path, "w") as outfile:
+            json.dump(existing_data, outfile, indent=4)
 
     return render_template("index.html", corrected_text=corrected_text, suggestions=suggestions, image_url=image_url, blip_beam_text=blip_beam_text, context=context, article_name=article_name, caption=caption)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="127.0.0.1", debug=True)
