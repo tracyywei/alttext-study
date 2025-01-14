058e436e262266b299f222e8b56ac11350150383
Tracy Wei <tracywei27@gmail.com>
2025-01-13 20:32:39 -0800

    updated for study 2025

--- app.py
+++ app.py
@@ -3,103 +3,81 @@ import os
 import random
 import pandas as pd
 from flask import Flask, request, render_template, redirect
-from preprocessing import preprocess
-from suggestions import applySugg
 import time
+from openai import OpenAI
+import re
 
 app = Flask(__name__, template_folder='templates')
 data_folder = os.path.join(app.root_path, 'data')  # Path to the 'data' folder
 
 # Function to read a random row from the CSV file
 def get_random_data():
-    csv_file = os.path.join(data_folder, 'generated_captions_web.csv')
+    csv_file = os.path.join(data_folder, 'output_alt_texts_full.csv')
     df = pd.read_csv(csv_file)
     random_row = df.sample(n=1)
-    article_name = random_row['articlenames'].values[0]
-    image_url = random_row['image'].values[0]
-    blip_beam_text = random_row['blip_beam'].values[0]
-    context = random_row['context'].values[0]
-    caption = random_row['caption'].values[0]
-    return image_url, blip_beam_text, context, article_name, caption
-
+    return random_row
 
 @app.route('/', methods=["GET", "POST"])
+def startTutorial():
+    prolific_pid = request.args.get("PROLIFIC_PID", "")
+    return render_template("tutorial.html", prolific_pid=prolific_pid)
+
+@app.route('/eval', methods=["GET", "POST"])
 def getText():
-    image_url, blip_beam_text, context, article_name, caption = get_random_data()
-    start_time = time.time()
+    prolific_pid = request.args.get("PROLIFIC_PID", "")
+    random_row = get_random_data()
+    return render_template("index.html",
+                            image_url=random_row['image_url'].values[0], 
+                            context=(re.sub(r'\[\d+\]', '', random_row['context'].values[0]))[:1000],
+                            article_name=random_row['article_title'].values[0],
+                            no_crt_no_cnxt=random_row['no_crt_no_cnxt'].values[0].replace("Alt-text: ", ""),
+                            no_crt_yes_cnxt=random_row['no_crt_yes_cnxt'].values[0].replace("Alt-text: ", ""),
+                            yes_crt_no_cnxt=random_row['yes_crt_no_cnxt'].values[0].replace("Alt-text: ", ""),
+                            yes_crt_yes_cnxt=random_row['yes_crt_yes_cnxt'].values[0].replace("Alt-text: ", "")
+                        )
 
-    # preprocessing
-    myText = preprocess(blip_beam_text)
-
-    # analyze text for suggestions
-    corrected_text, suggestions = applySugg(myText, context)
+@app.route('/nextImg', methods=["GET", "POST"])
+def nextImg():
 
     if request.method == "POST":
-
-        orig_text = request.form.get("ogText", "")
-        edited_text = request.form.get("editedText", "")
-        image = request.form.get("ogImg", "")
-
-        end_time = time.time()
+        responses = request.json
+        prolific_pid = responses.pop('prolificPID', None)
+        if prolific_pid:
+            responses['prolificPID'] = prolific_pid
+        img_url = responses.pop('img_url', None)
+        if img_url:
+            responses['img_url'] = img_url
+        article_name = responses.pop('article_name', None)
+        if img_url:
+            responses['article_name'] = article_name
+        alttext_type = responses.pop('alttext_type', None)
+        if img_url:
+            responses['alttext_type'] = alttext_type
 
         dictionary = {
-            "start_time": start_time,
-            "end_time": end_time,
-            "orig_text": orig_text,
-            "edited_text": edited_text,
-            "image": image
+            "pid": prolific_pid,
+            "img_url": img_url,
+            "alttext_type": alttext_type,
+            "article_name": article_name,
         }
         
-        # Writing to json
-        saved_file_path = os.path.join(data_folder, 'study.json')
+        saved_file_path = os.path.join(data_folder, 'study_responses.json')
         
-        # Load existing data from the file if available
         existing_data = []
         if os.path.exists(saved_file_path):
             with open(saved_file_path, "r") as infile:
                 existing_data = json.load(infile)
         
-        # Append the new data entry
         existing_data.append(dictionary)
         
-        # Write the updated data back to the file
-        with open(saved_file_path, "w") as outfile:
-            json.dump(existing_data, outfile, indent=4)
-
-    return render_template("index.html", corrected_text=corrected_text, suggestions=suggestions, image_url=image_url, blip_beam_text=blip_beam_text, context=context, article_name=article_name, caption=caption)
-
-@app.route('/skip', methods=["GET", "POST"])
-def skip():
-
-    if request.method == "POST":
-
-        orig_text = request.form.get("ogText-skip", "")
-        image = request.form.get("ogImg-skip", "")
-        reason = request.form['options']
-
-        dictionary = {
-            "orig_text": orig_text,
-            "image": image,
-            "reason": reason
-        }
-        
-        # Writing to json
-        saved_file_path = os.path.join(data_folder, 'skipped.json')
-        
-        # Load existing data from the file if available
-        existing_data = []
-        if os.path.exists(saved_file_path):
-            with open(saved_file_path, "r") as infile:
-                existing_data = json.load(infile)
-        
-        # Append the new data entry
-        existing_data.append(dictionary)
-        
-        # Write the updated data back to the file
         with open(saved_file_path, "w") as outfile:
             json.dump(existing_data, outfile, indent=4)
 
         return redirect('/')
+
+@app.route('/complete')
+def studyComplete():
+    return render_template('confirmation.html')
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", debug=True)
